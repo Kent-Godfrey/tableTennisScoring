95a597e1946b2445d24d8864e436ab084d531544
Kent Godfrey <kenny_g1996@hotmail.co.uk>
2018-09-23 23:00:40 +0100

    commit all files up to date. Mid development

---
+++ js/script.js
@@ -0,0 +1,97 @@
+let redGameScore = 0;
+let blueGameScore = 0;
+let redSetScore = 0;
+let blueSetScore = 0;
+//let totalScore = redGameScore + blueGameScore;
+let totalScore;
+let serviceTurn = true;
+const redAddBtn = document.getElementById("redPlus");
+const blueAddBtn = document.getElementById("bluePlus");
+const redMinusBtn = document.getElementById("redMinus");
+const blueMinusBtn = document.getElementById("blueMinus");
+
+function addRedPoint() {
+    ++redGameScore;
+    serviceOrder();
+    document.getElementById("redMatchScore").innerHTML = redGameScore;
+
+}
+
+function addBluePoint() {
+    ++blueGameScore;
+    serviceOrder();
+    document.getElementById("blueMatchScore").innerHTML = blueGameScore;
+
+}
+
+function minusRedPoint() {
+
+    --redGameScore;
+    serviceOrder();
+    document.getElementById("redMatchScore").innerHTML = redGameScore;
+
+}
+
+function minusBluePoint() {
+
+    --blueGameScore;
+    serviceOrder();
+    document.getElementById("blueMatchScore").innerHTML = blueGameScore;
+
+}
+
+/*
+function serviceOrder() {
+    totalScore = redGameScore + blueGameScore;
+
+    if ((redGameScore >= 10 && blueGameScore >= 10) || totalScore % 2 == 0)
+
+        serviceTurn = !serviceTurn;
+    console.log(serviceTurn);
+
+    //if serviceTurn true, then yellow else white,
+    //if serviceTurn true, then white, else yellow
+    document.getElementById("redMatchScore").style.color = serviceTurn ? "yellow" : "white";
+    document.getElementById("blueMatchScore").style.color = serviceTurn ? "white" : "yellow";
+}*/
+
+
+function serviceOrder() {
+    totalScore = redGameScore + blueGameScore;
+
+    if (redGameScore <= 10 || blueGameScore <= 10) {
+        if (totalScore % 4 >= 2) {
+            serviceTurn = true;
+            document.getElementById("blueMatchScore").style.color = "white";
+            document.getElementById("redMatchScore").style.color = "yellow";
+
+
+            //document.getElementById("blueSetScore").classList.toggle("hidden");
+            document.getElementById("redMatchScore").style.color = "yellow";
+        } else {
+            serviceTurn = false;
+            document.getElementById("redMatchScore").style.color = "white";
+            document.getElementById("blueMatchScore").style.color = "yellow";
+        }
+    }
+    if (redGameScore >= 10 && blueGameScore >= 10) {
+        if (totalScore % 2 == 0) {
+            serviceTurn = true;
+            document.getElementById("blueMatchScore").style.color = "white";
+            document.getElementById("redMatchScore").style.color = "yellow";
+
+        } else {
+            document.getElementById("redMatchScore").style.color = "white";
+            document.getElementById("blueMatchScore").style.color = "yellow";
+        }
+    }
+
+}
+
+
+
+
+redAddBtn.addEventListener("click", addRedPoint);
+blueAddBtn.addEventListener("click", addBluePoint);
+redMinusBtn.addEventListener("click", minusRedPoint);
+blueMinusBtn.addEventListener("click", minusBluePoint);
